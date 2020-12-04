5278e0e4471053d4f027dd612594cf64ec6f48b1
gilmarferrini <ferrinigilmar@gmail.com>
2020-12-04 15:35:25 -0300

    Added new method(insertNewItem)

--- app/src/main/java/com/example/myshoppingcart/view/MainActivity.java
+++ app/src/main/java/com/example/myshoppingcart/view/MainActivity.java
@@ -3,14 +3,35 @@ package com.example.myshoppingcart.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.myshoppingcart.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editTextItem;
+    Button buttonSaveItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        buttonSaveItem = findViewById(R.id.buttonSaveItem);
+
+        buttonSaveItem.setOnClickListener(v -> {
+            insertNewItem();
+        });
+
     }
+
+    public void insertNewItem() {
+        editTextItem = findViewById(R.id.editTextItem);
+        Toast.makeText(getApplicationContext(), editTextItem.getText().toString(), Toast.LENGTH_LONG).show();
+    }
+
 }
