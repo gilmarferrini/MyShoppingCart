8c286697326a2b391d51bf2c16e1d121b099db17
gilmarferrini <ferrinigilmar@gmail.com>
2020-12-05 10:12:10 -0300

    Added execSQL

--- app/src/main/java/com/example/myshoppingcart/datasource/AppDataBase.java
+++ app/src/main/java/com/example/myshoppingcart/datasource/AppDataBase.java
@@ -6,18 +6,24 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.example.myshoppingcart.datamodel.ItemDataModel;
+
 public class AppDataBase extends SQLiteOpenHelper {
 
     public static final String DB_NAME = "myshoppingcart.sqlite";
     public static final int DB_VERSION = 1;
 
+    private SQLiteDatabase db;
+
     public AppDataBase(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
+
+        db = getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(ItemDataModel.createTable());
     }
 
     @Override
