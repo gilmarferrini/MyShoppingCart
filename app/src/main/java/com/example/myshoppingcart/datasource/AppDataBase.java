45e029bbf3c3e556ccb48c2f2ae173e85328e445
gilmarferrini <ferrinigilmar@gmail.com>
2020-12-06 10:32:40 -0300

    added index method

--- app/src/main/java/com/example/myshoppingcart/datasource/AppDataBase.java
+++ app/src/main/java/com/example/myshoppingcart/datasource/AppDataBase.java
@@ -1,12 +1,19 @@
 package com.example.myshoppingcart.datasource;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 import com.example.myshoppingcart.datamodel.ItemDataModel;
+import com.example.myshoppingcart.model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AppDataBase extends SQLiteOpenHelper {
 
@@ -30,4 +37,47 @@ public class AppDataBase extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
+    public boolean insert(String tableName, ContentValues data) {
+        boolean result = false;
+
+        db = getWritableDatabase();
+
+        try {
+            result = db.insert(tableName, null, data) > 0;
+        } catch (Exception e) {
+            Log.e("TAG", "AppDataBase insert: " + e.getMessage());
+        }
+
+        return result;
+    }
+
+    public List<Item> index(String tableName) {
+        List<Item> list = new ArrayList<>();
+
+        db = getWritableDatabase();
+        String query = "SELECT * FROM " + tableName;
+
+        Cursor cursor;
+        cursor = db.rawQuery(query, null);
+
+        try {
+            if(cursor.moveToFirst()) {
+                do {
+                    Item item = new Item();
+                    item.setId(cursor.getInt(cursor.getColumnIndex(ItemDataModel.ID)));
+                    item.setName(cursor.getString(cursor.getColumnIndexOrThrow(ItemDataModel.NAME)));
+
+                    list.add(item);
+
+                } while(cursor.moveToNext());
+            }
+
+        } catch (Exception e) {
+            Log.e("TAG", "AppDataBase insert: " + e.getMessage());
+        }
+
+        return list;
+    }
+
 }
